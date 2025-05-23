4e44957d2529389e322a4172ddff1dfd159bce69
Aidanvb5 <152182270+Aidanvb5@users.noreply.github.com>
2025-05-23 15:44:25 +0200

    work pls

--- Web/next.config.js
+++ Web/next.config.js
@@ -3,7 +3,7 @@ const repo = 'Gebarentaal_vertaler.github.io';
 
 module.exports = {
   output: 'export',
-  distDir: 'docs',
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+  distDir: '../docs',
+  basePath: '/Gebarentaal_vertaler.github.io',
+  assetPrefix: '/Gebarentaal_vertaler.github.io/',
 };
