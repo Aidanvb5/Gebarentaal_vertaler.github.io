60ea3f6bb3cd3752db7ef3ee8c8ab4f964e0679c
Aidanvb5 <152182270+Aidanvb5@users.noreply.github.com>
2025-05-23 15:39:41 +0200

    should work now

--- Web/next.config.js
+++ Web/next.config.js
@@ -1,9 +1,9 @@
 // next.config.js for static export to 'docs' for GitHub Pages
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repo = 'Gebarentaal_vertaler.github.io';
 
 module.exports = {
   output: 'export',
   distDir: 'docs',
-  basePath: isGithubPages ? '/Gebarentaal_vertaler.github.io' : '',
-  assetPrefix: isGithubPages ? '/Gebarentaal_vertaler.github.io/' : '',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
